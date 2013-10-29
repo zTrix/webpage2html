c05a2d4ee5d1c7678ff4754846ef90a4eb8a7368
zTrix <i@ztrix.me>
2013-10-29 23:59:12 +0800

    handle http url

--- webpage2html.py
+++ webpage2html.py
@@ -1,28 +1,84 @@
 #!/usr/bin/env python2
 
-import os, sys, requests, re, base64
+import os, sys, requests, re, base64, httplib, urlparse
 from bs4 import BeautifulSoup
 
 re_css_url = re.compile('(url\(.*?\))')
 
-def get(base, relpath = None):
-    if base.startswith('http'):
-        raise Exception('Not implemented')
-    elif os.path.exists(base):
+def get(index, relpath = None):
+    print >> sys.stderr, index, relpath
+    if index.startswith('http'):
+        parsed_url = urlparse.urlparse(index)
+        if parsed_url.scheme == 'http':
+            conn = httplib.HTTPConnection(parsed_url.netloc)
+        elif parsed_url.scheme == 'https':
+            conn = httplib.HTTPSConnection(parsed_url.netloc)
+        else:
+            raise Exception('unsupported scheme: ' + parsed_url.scheme)
+        urlpath = parsed_url.path
+        if relpath:
+            if relpath.startswith('/'):
+                urlpath = relpath
+            else:
+                urlpath = os.path.join(urlpath, relpath)
+        conn.request('GET', urlpath, None, {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)'})
+        return conn.getresponse().read()
+    elif os.path.exists(index):
         if relpath:
             return open(relpath).read()
         else:
-            return open(base).read()
+            return open(index).read()
     else:
-        raise Exception('cannot get web content for %s', base)
+        raise Exception('cannot get web content for %s', index)
 
-def generate(base):
-    html_doc = get(base)
+def image_to_base64(index, src):
+    # doc here: http://en.wikipedia.org/wiki/Data_URI_scheme
+    if src.strip().startswith('data:'):
+        return src
+    if src.lower().endswith('png'):
+        fmt = 'png'
+    elif src.lower().endswith('gif'):
+        fmt = 'gif'
+    elif src.lower().endswith('jpg') or src.lower().endswith('jpeg'):
+        fmt = 'jpg'
+    else:
+        fmt = 'png'
+    return ('data:image/%s;base64,' % fmt) + base64.b64encode(get(index, src))
+
+def handle_css_content(index, css):
+    if not css:
+        return css
+    # Watch out! how to handle urls which contain parentheses inside? Oh god, css does not support such kind of urls
+    # I tested such url in css, and, unfortunately, the css rule is broken. LOL!
+    # I have to say that, CSS is awesome!
+    reg = re.compile(r'url\s*\((.+?)\)')
+    def repl(matchobj):
+        src = matchobj.group(1).strip(' \'"')
+        return 'url (' + image_to_base64(index, src) + ')'
+    css = reg.sub(repl, css)
+    return css
+
+def generate(index):
+    html_doc = get(index)
+    # since BeautifulSoup will handle unclosed tags like <meta> and <link>, add a closing tag which we don't need
+    # we should add the closing tag first by ourselves.
+    def repl(matchobj):
+        es = matchobj.group(1)
+        if len(es) > 2 and es[-2] != '/':
+            return es[:-1] + ' />'
+        return es
+    html_doc = re.sub(r'(<\s*meta.+?>)', repl, html_doc)
+    html_doc = re.sub(r'(<\s*link.+?>)', repl, html_doc)
+
+    # now build the dom tree
     soup = BeautifulSoup(html_doc)
-    for link in soup('link', type='text/css'):
-        if link['href']:
-            css = soup.new_tag('style', type = 'text/css')
-            css.string = get(base, link['href'])
+    for link in soup('link'):
+        if link.has_attr('type') and link['type'] != 'text/css': continue
+        if link.has_attr('href') and link['href'] and link['href'].lower().endswith('.css'):
+            new_type = 'text/css' if not link.has_attr('type') or not link['type'] else link['type']
+            css = soup.new_tag('style', type = new_type)
+            # print >> sys.stderr, link['href']
+            css.string = get(index, link['href'])
             #pos = 0
             #while True:
             #    print pos
@@ -32,30 +88,34 @@ def generate(base):
             #    print m.group(0), m.start(0), m.end(0)
             #    pos = m.end(0)
             link.replace_with(css)
-    for js in soup('script', type='text/javascript'):
-        if 'src' not in js or not js['src']:
+    for js in soup('script'):
+        if not js.has_attr('src') or not js['src']:
             continue
-        code = soup.new_tag('script', type='text/javascript')
-        code.string = get(base, js['src'])
-        js.repace_with(code)
+        new_type = 'text/javascript' if not js.has_attr('type') or not js['type'] else js['type']
+        code = soup.new_tag('script', type=new_type)
+        code.string = get(index, js['src'])
+        #print >> sys.stderr, js is None, code is None, type(js), type(code), len(code.string)
+        js.replace_with(code)
     for img in soup('img'):
-        if 'src' not in img: continue
-        src = img['src']
-        if src.lower().endswith('png'):
-            fmt = 'png'
-        elif src.lower().endswith('gif'):
-            fmt = 'gif'
-        elif src.lower().endswith('jpg') or src.lower().endswith('jpeg'):
-            fmt = 'jpg'
-        else:
-            fmt = 'png'
-        img['src'] = ('data:image/%s;base64,' % fmt) + base64.b64encode(get(base, src))
+        if not img.has_attr('src') or not img['src']: continue
+        img['src'] = image_to_base64(index, img['src'])
+    for tag in soup(True):
+        if tag.has_attr('style'):
+            if tag['style']:
+                tag['style'] = handle_css_content(index, tag['style'])
+        elif tag.name == 'link' and tag.has_attr('type') and tag['type'] == 'text/css':
+            if tag.string:
+                tag.string = handle_css_content(index, tag.string)
+        elif tag.name == 'style':
+            if tag.string:
+                tag.string = handle_css_content(index, tag.string)
     return soup.prettify(formatter='html')
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print 'usage: %s <input html>' % sys.argv[0]
+        print 'usage: %s <saved html>|<index url>' % sys.argv[0]
         sys.exit(10)
-    os.chdir(os.path.dirname(sys.argv[1]))
+    if os.path.dirname(sys.argv[1]) and os.path.exists(sys.argv[1]):
+        os.chdir(os.path.dirname(sys.argv[1]))
     sys.stdout.write(generate(sys.argv[1]).encode('utf8'))
 
