42eefbfa7077c0ac6223322cd80046bbed74407a
zTrix <i@ztrix.me>
2014-02-24 15:27:55 +0800

    better and colored log output

--- webpage2html.py
+++ webpage2html.py
@@ -6,6 +6,21 @@ import lxml
 
 re_css_url = re.compile('(url\(.*?\))')
 
+try:
+    from termcolor import colored
+except:
+    def colored(text, color=None, on_color=None, attrs=None):
+        return text
+
+def log(s, color = None, on_color = None, attrs = None, new_line = True):
+    if not color:
+        print >> sys.stderr, str(s),
+    else:
+        print >> sys.stderr, colored(str(s), color, on_color, attrs),
+    if new_line:
+        sys.stderr.write('\n')
+    sys.stderr.flush()
+
 def absurl(index, relpath = None):
     if index.lower().startswith('http') or (relpath and relpath.startswith('http')):
         parsed_url = urlparse.urlparse(index)
@@ -25,23 +40,26 @@ def absurl(index, relpath = None):
     else:
         return os.path.normpath(os.path.join(os.path.dirname(index), relpath))
 
-def get(index, relpath = None):
+def get(index, relpath = None, verbose = True):
     if index.startswith('http') or (relpath and relpath.startswith('http')):
         fullpath = absurl(index, relpath)
         if not fullpath:
-            print >> sys.stderr, 'Warning: invalid path', index, relpath
+            log('[ WARN ] invalid path, %s %s' % (index, relpath), 'yellow')
             return ''
-        print >> sys.stderr, fullpath
         request = urllib2.Request(fullpath)
         request.add_header('User-Agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)')
         try:
             response = urllib2.urlopen(request)
+            if verbose:
+                log('[ GET ] 200 - %s' % fullpath)
             return response.read()
         except urllib2.HTTPError, err:
-            print >> sys.stderr, 'Warning: HTTPError while open ' + fullpath, err
+            if verbose:
+                log('[ WARN ] %d - %s %s' % (err.code, fullpath, err.reason), 'yellow')
             return ''
         except urllib2.URLError, err:
-            print >> sys.stderr, 'Warning: URLError while open ' + fullpath, err
+            if verbose:
+                log('[ WARN ] URLError - %s %s' % (fullpath, err.reason), 'yellow')
             return ''
             
     elif os.path.exists(index):
@@ -50,17 +68,23 @@ def get(index, relpath = None):
                 fullpath = relpath
             else:
                 fullpath = os.path.normpath(os.path.join(os.path.dirname(index), relpath))
-            print >> sys.stderr, fullpath
             try:
-                return open(fullpath).read()
+                ret = open(fullpath).read()
+                if verbose: log('[ LOCAL ] found - %s' % fullpath)
+                return ret
             except IOError, err:
-                print >> sys.stderr, 'Warning: cannot find file', fullpath, err
+                if verbose: log('[ WARN ] file not found - %s %s' % (fullpath, str(err)), 'yellow')
                 return ''
         else:
-            print >> sys.stderr, index
-            return open(index).read()
+            try:
+                ret = open(index).read()
+                if verbose: log('[ LOCAL ] found - %s' % index)
+                return ret
+            except IOError, err:
+                if verbose: log('[ WARN ] file not found - %s %s' % (index, str(err)), 'yellow')
+                return ''
     else:
-        print >> sys.stderr, 'Warning: cannot get web content for', index
+        if verbose: log('[ ERROR ] invalid index - %s' % index, 'red')
         return ''
 
 def image_to_base64(index, src):
@@ -97,12 +121,12 @@ def handle_css_content(index, css):
     css = reg.sub(repl, css)
     return css
 
-def generate(index):
+def generate(index, verbose = True):
     '''
     given a index url such as http://www.google.com, http://custom.domain/index.html
     return generated single html 
     '''
-    html_doc = get(index)
+    html_doc = get(index, verbose = verbose)
 
     # now build the dom tree
     soup = BeautifulSoup(html_doc, 'lxml')
@@ -113,8 +137,7 @@ def generate(index):
             if link['href'].lower().startswith('http://fonts.googleapis.com'): continue
             new_type = 'text/css' if not link.has_attr('type') or not link['type'] else link['type']
             css = soup.new_tag('style', type = new_type)
-            # print >> sys.stderr, link['href']
-            new_css_content = handle_css_content(absurl(index, link['href']), get(index, link['href']))
+            new_css_content = handle_css_content(absurl(index, link['href']), get(index, relpath = link['href'], verbose = verbose))
             if False: # new_css_content.find('@font-face') > -1 or new_css_content.find('@FONT-FACE') > -1:
                 link['href'] = 'data:text/css;base64,' + base64.b64encode(new_css_content)
             else:
@@ -126,7 +149,7 @@ def generate(index):
         new_type = 'text/javascript' if not js.has_attr('type') or not js['type'] else js['type']
         code = soup.new_tag('script', type=new_type)
         try:
-            js_str = get(index, js['src'])
+            js_str = get(index, relpath = js['src'], verbose = verbose)
             if js_str.find('</script>') > -1:
                 code['src'] = 'data:text/javascript;base64,' + base64.b64encode(js_str)
             elif js_str.find(']]>') < 0:
@@ -137,13 +160,20 @@ def generate(index):
                 # code.string = '<![CDATA[\n' + js_str.replace(']]>', ']]]]><![CDATA[>') + '\n]]>'
                 code.string = js_str
         except:
-            print >> sys.stderr, repr(js_str)
+            log(repr(js_str))
             raise
         #print >> sys.stderr, js is None, code is None, type(js), type(code), len(code.string)
         js.replace_with(code)
     for img in soup('img'):
         if not img.has_attr('src') or not img['src']: continue
         img['src'] = image_to_base64(index, img['src'])
+        def check_alt(attr):
+            if img.has_attr(attr) and img[attr].startswith('this.src='):
+                # we do not handle this situation yet, just warn the user
+                if verbose: log('[ WARN ] %s found in img tag and unhandled, which may break page' % (attr), 'yellow')
+        check_alt('onerror')
+        check_alt('onmouseover')
+        check_alt('onmouseout')
     for tag in soup(True):
         if tag.has_attr('style'):
             if tag['style']:
@@ -154,6 +184,15 @@ def generate(index):
         elif tag.name == 'style':
             if tag.string:
                 tag.string = handle_css_content(index, tag.string)
+
+    # TODO, add prefix here
+    """
+    <!--
+     single html processed by https://github.com/zTrix/webpage2html
+     url: http://xxx
+     saved date: Mon Feb 24 2014 02:06:40 GMT+0800 (CST) 
+    -->
+    """
     return soup.prettify(formatter='html')
 
 if __name__ == '__main__':
